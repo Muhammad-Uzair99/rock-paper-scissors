56642f75de23ce2792e151da535dc04f48a7f0ef
Muhammad-Uzair99 <uzairmaster97@gmail.com>
2023-12-10 16:06:33 +0500

    Add header with relivent content

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'text': '#d9dbf9',
+        'background': '#000016',
+        'primary': '#8d92ed',
+        'secondary': '#4f1588',
+        'accent': '#9922dd',
+      },
+      fontFamily: {
+        'poppins': ['Poppins'],
+      },
+    },
+  },
+  plugins: [],
+}
+
